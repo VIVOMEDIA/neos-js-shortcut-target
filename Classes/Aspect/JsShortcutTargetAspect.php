467384aaff344ec85fe0e91561685bace0434774
Denny Lubitz <lubitz@vivomedia.de>
2022-05-12 09:33:50 +0200

    Initial commit

---
+++ Classes/Aspect/JsShortcutTargetAspect.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace VIVOMEDIA\JsShortcutTarget\Aspect;
+
+use Neos\ContentRepository\Domain\Model\Node;
+use Neos\Flow\Aop\JoinPointInterface;
+
+/**
+ * @Flow\Scope("singleton")
+ * @Flow\Aspect
+ */
+class JsShortcutTargetAspect
+{
+    /**
+     * @Flow\Around("method(Neos\Flow\Mvc\Routing\UriBuilder->uriFor())")
+     * @param \Neos\Flow\Aop\JoinPointInterface $joinPoint
+     * @return string
+     */
+    public function rewritePluginViewUris(JoinPointInterface $joinPoint)
+    {
+        $arguments = $joinPoint->getMethodArguments();
+
+        $node = $arguments['controllerArguments']['node'] ?? null;
+        if ($node && $node instanceof Node && $node->getNodeType()->isOfType('Neos.Neos:Shortcut')) {
+            $target = $node->getProperty('target');
+            if (strpos($target, 'javascript:') !== false) {
+                return $target;
+            }
+        }
+        return $joinPoint->getAdviceChain()->proceed($joinPoint);
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# Neos CMS FontAwesome Pugin
+
+[![Latest Stable Version](https://poser.pugx.org/vivomedia/neos-js-shortcut-target/v/stable)](https://packagist.org/packages/vivomedia/neos-fontawesome-icon)
+[![Total Downloads](https://poser.pugx.org/vivomedia/neos-js-shortcut-target/downloads)](https://packagist.org/packages/vivomedia/neos-fontawesome-icon)
+[![License](https://poser.pugx.org/vivomedia/neos-js-shortcut-target/license)](https://packagist.org/packages/vivomedia/neos-fontawesome-icon)
+
+Allows setting `javascript:` as target url in Shortcut Nodetypes.
+
+Example: 
+
+```
+javascript:openCookieBannerConfiguration();
+```
+
+## Install
+
+Install with composer
+
+```
+composer require vivomedia/neos-js-shortcut-target 
+```
